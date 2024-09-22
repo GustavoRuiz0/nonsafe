63cf22ffa9581876dc558d06f55ce03aaebe4b52
gustavoruiz <gustavolvruiz8@gmail.com>
2024-09-21 23:15:32 -0300

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+console.warn = () => {};
+console.error = () => {};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
